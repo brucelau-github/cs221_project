6f6990aaf2d79f291816b2edda27c57f028150f9
Bruce Liu <jianey.liu@oracle.com>
2022-05-15 02:35:26 -0400

    chess step is_win

--- chess.py
+++ chess.py
@@ -1,4 +1,7 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+import gym
 
 # Colors
 COLOR_AC_BUTTON = (200, 200, 0)
@@ -10,19 +13,78 @@ COLOR_GREEN = (0, 200, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
-class Gomoku:
-    def __init__(self):
+class Gomoku(gym.Env):
+    def __init__(self, n=15):
+        super().__init__()
+        self.players = [1, -1]
+        self.next_player = 1
+        self.action_space = gym.spaces.Discrete(n*n)
+        self.observation_space = gym.spaces.Discrete(n*n)
+        self.board_size = n
+        self.stone = {-1:[], 1:[]}
+        self.score = {-1:0, 1:0}
+        self.chess_board = [[0]*n for _ in range(n)]
+
+    def reset(self):
+        """ reset the chess status """
+        self.stone = {-1:[], 1:[]}
+        self.score = {-1:0, 1:0}
+        self.chess_board = [[0]*n for _ in range(n)]
+
+    def step(self, action):
+        """ place a piece on the board """
+        m, n = action
+        observation, reward, done, info = self.chess_board, -1, False, {}
+        player = self.next_player
+        self.chess_board[m][n] = player
+        self.stone[player].append(action)
+        is_win = self.is_win(self.stone[player])
+        if is_win: 
+            reward = 5000
+            done = True
+        self.next_player *= -1
+        self.score[player] += reward
+        return (observation, reward, done, info)
+
+    def is_win(self, stone):
+        if len(stone) <= 5:
+            return False
+
+        stone_sort = sorted(stone)
+        for x, y in stone_sort:
+            row, col, diag, adiag = [], [], [], []
+            for i in range(1, 5):
+                row.append((x, y+i))
+                col.append((x+i, y))
+                diag.append((x+i, y+i))
+                adiag.append((x+i, y-i))
+            stone_set = set(stone_sort)
+            win = (stone_set.issuperset(set(row))
+                   or stone_set.issuperset(set(col))
+                   or stone_set.issuperset(set(diag))
+                   or stone_set.issuperset(set(adiag)))
+            return win
+
+    def print_info(self):
+        print(self.stone)
+        print(self.next_player)
+        print(self.chess_board)
+
+    def render(self):
+        """ render a chess board """
         self.w_h = 675
-        self.title = "GOMOKU for 2 players"
         self.screen = pygame.display.set_mode((900, self.w_h + 45))
-        pygame.display.set_caption(self.title)
+        pygame.display.set_caption("GOMOKU")
         self.screen.fill(COLOR_BOARD)
+        return None
 
-    def draw_main(self, x=45*16, y=45, w=125, h=45,
-                  button_color=COLOR_BUTTON, ac_button_color=COLOR_AC_BUTTON):
+    def draw_board(self):
 
+        h_line = pygame.draw.line(self.screen, COLOR_BLACK, [45, 45], [45, 675], 2)
+        v_line = pygame.draw.line(self.screen, COLOR_BLACK, [45, 45], [675, 45], 2)
         # Gomoku board.
-        for i in range(1, 16):
+        for i in range(1, self.board_size+1):
+            h_line.copy().move(45, 0)
             pygame.draw.line(self.screen, COLOR_BLACK,
                              [45 * i, 45], [45 * i, self.w_h], 2)
             pygame.draw.line(self.screen, COLOR_BLACK,
@@ -199,85 +261,21 @@ class Gomoku:
 
 if __name__ == "__main__":
     pygame.init()
-    pygame.font.init()
-
-    stone = {}
-    stone["white"], stone["black"] = [], []
-    player1_score, player2_score = 0, 0
     game = Gomoku()
-    game.draw_main()
-    game.draw_score(player1_score, player2_score)
-
-    play_order = None
-
-    while True:
-        event = pygame.event.wait()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-
-            x_stone, y_stone = game.play_get_pos()
-
-            # New game.
-            if (125 + 45 * 16) > x_stone > 45 * 16 and 90 > y_stone > 45:
-                stone["white"], stone["black"] = [], []
-                player1_score, player2_score = 0, 0
-                game = Gomoku()
-                game.draw_main()
-                game.draw_score(player1_score, player2_score)
-                game.text_draw("GAME START", game.w_h//2, 30, COLOR_GREEN, 35)
-                play_order = True
-
-            # Next game.
-            if (125 + 45 * 16) > x_stone > 45 * 16 and 160 > y_stone > 115:
-                stone["white"], stone["black"] = [], []
-                game = Gomoku()
-                game.draw_main()
-                game.draw_score(player1_score, player2_score)
-                game.text_draw("NEXT GAME START", game.w_h//2, 30, COLOR_GREEN, 35)
-                play_order = True
-
-            # Draw a white stone (Player 1).
-            if play_order is None:
-                pass
-            elif not play_order:
-                game.text_draw("PLAYER 1", 45 * 16 + 65, game.w_h // 2 - 90,
-                               COLOR_RED, 20)
-                game.text_draw("PLAYER 2", 45 * 16 + 65, game.w_h // 2 + 20,
-                               COLOR_BLACK, 20)
-                if 45 <= x_stone <= game.w_h and 45 <= y_stone <= game.w_h:
-                    x_stone, y_stone = game.play_draw_stone_pos()
-                    stone, play_order = game.play_draw_stone(
-                        stone, play_order, "white", COLOR_WHITE,
-                        x_stone, y_stone)
-                    game.text_draw("PLAYER 1", 45 * 16 + 65, game.w_h // 2 - 90,
-                                   COLOR_GRAY, 20)
-                    game.text_draw("PLAYER 2", 45 * 16 + 65, game.w_h // 2 + 20,
-                                   COLOR_RED, 20)
-                    player1_score, play_order = game.score(
-                        stone, "white", player1_score, play_order)
-                    if len(stone["white"]) + len(stone["black"]) == 225:
-                        game.text_draw("DRAW", 45 * 16 + 65, game.w_h // 2 + 120,
-                                       (200, 0, 0), 45)
-                        play_order = None
-
-            # Draw a black stone (Player 2).
-            elif play_order:
-                game.text_draw("PLAYER 1", 45 * 16 + 65, game.w_h // 2 - 90,
-                               COLOR_GRAY, 20)
-                game.text_draw("PLAYER 2", 45 * 16 + 65, game.w_h // 2 + 20,
-                               COLOR_RED, 20)
-                if 45 <= x_stone <= game.w_h and 45 <= y_stone <= game.w_h:
-                    x_stone, y_stone = game.play_draw_stone_pos()
-                    stone, play_order = game.play_draw_stone(
-                        stone, play_order, "black", COLOR_BLACK, x_stone, y_stone)
-                    game.text_draw("PLAYER 1", 45 * 16 + 65, game.w_h // 2 - 90,
-                                   COLOR_RED, 20)
-                    game.text_draw("PLAYER 2", 45 * 16 + 65, game.w_h // 2 + 20,
-                                   COLOR_BLACK, 20)
-                    player2_score, play_order = game.score(
-                        stone, "black", player2_score, play_order)
-                    if len(stone["white"]) + len(stone["black"]) == 225:
-                        game.text_draw("DRAW", 45 * 16 + 65, game.w_h//2 + 120,
-                                       (200, 0, 0), 45)
-                        play_order = None
-        game.interactive_button()
-        pygame.display.update()
+    tests = [
+        [(0, 0)],
+        [(0, 0),(1, 1),(2, 2),(3, 3),(4, 4),(5, 5)],
+        [(0, 0),(1, 0),(2, 0),(3, 0),(4, 0),(5, 0)],
+        [(0, 0),(0, 1),(0, 2),(0, 3),(0, 4),(0, 5)],
+        [(5, 5),(6, 4),(7, 3),(8, 2),(9, 1),(10, 0)]
+    ]
+    for i in tests:
+        print(game.is_win(i))
+    memory1 = [(5, 5),(6, 4),(7, 3),(8, 2),(9, 1),(10, 0)]
+    memory2 = [(0, 0),(1, 0),(2, 0),(3, 0),(4, 0),(5, 0)]
+    for p1, p2 in zip(memory1, memory2):
+        s = game.step(p1)
+        print(f"reward:{s[1]}, done:{s[2]}")
+        game.print_info()
+        s = game.step(p2)
+        game.print_info()
